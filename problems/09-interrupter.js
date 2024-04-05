e81bdfe5dc5f6e3fa80e2573663e7ab1b1e8f817
Omega Dziko F. <megaoy@yahoo.com>
2024-04-04 20:42:48 -0400

    num 9 finished

--- problems/09-interrupter.js
+++ problems/09-interrupter.js
@@ -16,7 +16,41 @@ console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 ***********************************************************************/
 
-// Your code here 
+//declare interrupter func with param (interruptingWord)
+//return func and inside func
+const interrupter = (interruptingWord) => {
+
+  return (string) => {
+
+    let talked = [];
+
+    let strArr = string.split(' ');
+
+
+      for (let i = 0; i < strArr.length - 1; i++) {
+
+        let words = strArr[i];
+
+        talked.push(words + ' ' + interruptingWord);
+
+      }
+
+      talked.push(`${strArr[strArr.length - 1]}`);
+
+    return talked.join(' ');
+  }
+}
+
+
+//Look below to see how this function is invoked:
+let rudePerson = interrupter("what"); // => returns a function
+console.log(rudePerson("how are you")); // prints "how what are what you"
+console.log(rudePerson("I like pie")); // prints "I what like what pie"
+
+
+//Invoking the interrupter function again:
+let rudePerson2 = interrupter("yo"); // => returns a function
+console.log(rudePerson2("I love dogs")); // prints "I yo love yo dogs"
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
